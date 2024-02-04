7e71c460e50e4eceb6266af1cf6cc3e08861f174
sportshead <me@sportshead.dev>
2024-02-04 11:44:46 +0000

    remove default options, set titlebar to filename, add bang to remove preceding indent

--- lua/carbon-now.lua
+++ lua/carbon-now.lua
@@ -14,21 +14,7 @@ local language_map = {
 carbon.config = {
   base_url = "https://carbon.now.sh/",
   open_cmd = "xdg-open",
-  options = {
-    bg = "gray",
-    drop_shadow_blur = "68px",
-    drop_shadow = false,
-    drop_shadow_offset_y = "20px",
-    font_family = "Hack",
-    font_size = "18px",
-    line_height = "133%",
-    line_numbers = true,
-    theme = types.Themes.monokai,
-    titlebar = "Made with carbon-now.nvim",
-    watermark = false,
-    width = "680",
-    window_theme = types.WindowThemes.sharp,
-  },
+  options = {},
 }
 
 ---encodes a given [str] string
@@ -57,7 +43,9 @@ local function encode_params(values)
     if type(v) ~= "string" then
       v = tostring(v)
     end
-    table.insert(params, k .. "=" .. query_param_encode(v))
+    if v ~= "" and v ~= nil then
+      table.insert(params, k .. "=" .. query_param_encode(v))
+    end
   end
 
   return table.concat(params, "&")
@@ -70,6 +58,8 @@ end
 local function get_carbon_now_snapshot_uri(code)
   local opts = carbon.config.options
   local ft = vim.bo.filetype
+
+  local filename = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
 
   -- carbon.now.sh parameters
   local params = {
@@ -85,7 +75,7 @@ local function get_carbon_now_snapshot_uri(code)
     dsyoff = opts.drop_shadow_offset_y,
     dsblur = opts.drop_shadow_blur,
     wm = opts.watermark,
-    tb = opts.titlebar,
+    tb = filename,
     code = code,
   }
 
@@ -116,7 +106,7 @@ local function get_open_command()
   return "echo"
 end
 
----@param opts {args: string, line1: integer, line2: integer}
+---@param opts {args: string, line1: integer, line2: integer, bang?: boolean}
 local function create_snapshot(opts)
   -- get launch command
   local open_cmd = get_open_command()
@@ -130,6 +120,20 @@ local function create_snapshot(opts)
     url = carbon.config.base_url .. "/" .. opts.args .. "?" .. query_params
   else
     local range = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, false)
+    if opts.bang then
+      local common_indent = math.huge
+
+      for _, line in ipairs(range) do
+        local whitespace = line:match("^%s+")
+        if whitespace ~= nil then
+          common_indent = math.min(common_indent, #whitespace)
+        end
+      end
+
+      for i, line in ipairs(range) do
+        range[i] = line:sub(common_indent + 1)
+      end
+    end
     local code = table.concat(range, "\n", 1, #range)
     query_params = get_carbon_now_snapshot_uri(code)
     url = carbon.config.base_url .. "?" .. query_params
@@ -144,7 +148,7 @@ local function create_commands()
   ---@param opts table<string, any>
   vim.api.nvim_create_user_command("CarbonNow", function(opts)
     create_snapshot(opts)
-  end, { range = "%", nargs = "?" })
+  end, { range = "%", nargs = "?", bang = true })
 end
 
 --- initialization function for the carbon plugin commands
